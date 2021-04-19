1c211195c7b269f5458fb4475577c67021d81131
Mumtoz Kodirov <imumtoz@gmail.com>
2021-04-19 17:53:58 +0500

    Disabled location sync

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -25,7 +25,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('sync:location')->everyThirtyMinutes();
+//        $schedule->command('sync:location')->everyThirtyMinutes();
         $schedule->command('sync:nodes')->everySixHours();
     }
 
